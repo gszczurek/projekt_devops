73a99e20fffc876315c3f529c26cb12125f2cce3
Grzegorz Szczurek <gszczure@student.wszib.edu.pl>
2025-12-14 18:57:05 +0100

    fix: Change migration

---
+++ app/migrations/versions/bb2fe6e44774_initial_migration.py
@@ -0,0 +1,31 @@
+"""initial migration
+
+Revision ID: bb2fe6e44774
+Revises: 
+Create Date: 2025-12-14 17:56:25.433218
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'bb2fe6e44774'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("email", sa.String(120), nullable=False, unique=True),
+        sa.Column("name", sa.String(120), nullable=False),
+        sa.Column("created_at", sa.DateTime, nullable=False, server_default=sa.func.now())
+    )
+
+def downgrade() -> None:
+    op.drop_table("users")
